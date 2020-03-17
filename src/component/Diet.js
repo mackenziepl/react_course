86a56be236a4cd1dbd824701d64aafb7e8121ca5
mackenziepl <mackenziepl@poczta.onet.pl>
2020-03-17 15:39:16 +0100

    sort table

--- src/component/Diet.js
+++ src/component/Diet.js
@@ -2,19 +2,63 @@ import React, {Component} from "react";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import '../css/Diet.css';
+import EnhancedTableHead from "./EnhancedTableHeader";
 
 class Diet extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { rows: [], keys:""}
-
-        this.compareBy.bind(this);
-        this.sortBy.bind(this);
+        const headCells = [
+            {id: 'name', numeric: false, disablePadding: false, label: 'Dessert (100g serving)'},
+            {id: 'calories', numeric: true, disablePadding: false, label: 'Calories'},
+            {id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)'},
+            {id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)'},
+            {id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)'},
+        ];
+        this.state = {
+            rows: [],
+            headCells: headCells,
+            order: 'asc',
+            orderBy: 'calories',
+            selected: [],
+            page: 0,
+            rowsPerPage: 0
+        }
     }
+
+    handleRequestSort = (event, property) => {
+        const isAsc = this.state.orderBy === property && this.state.order === 'asc';
+        this.setState({order: isAsc ? 'desc' : 'asc'});
+        this.setState({orderBy: property});
+    };
+
+    stableSort = (array, comparator) => {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        return stabilizedThis.map(el => el[0]);
+    };
+
+    ascendingComparator = (a, b, orderBy) => {
+        if (b[orderBy] < a[orderBy]) {
+            return 1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return -1;
+        }
+        return 0;
+    };
+
+    getComparator = (order, orderBy) => {
+        return order === 'asc'
+            ? (a, b) => this.ascendingComparator(a, b, orderBy)
+            : (a, b) => -this.ascendingComparator(a, b, orderBy);
+    };
 
     componentDidMount() {
         fetch("data.txt")
@@ -23,36 +67,20 @@ class Diet extends Component {
             .catch(err => console.log(err));
     }
 
-    compareBy(key) {
-        return function (a, b) {
-            if (a[key] < b[key]) return -1;
-            if (a[key] > b[key]) return 1;
-            return 0;
-        };
-    }
-
-    sortBy(key) {
-        let arrayCopy = [...this.state.rows];
-        arrayCopy.sort(this.compareBy(key));
-        this.setState({rows: arrayCopy});
-    }
-
     render() {
         return (
             <div>
                 <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell onClick={() => this.sortBy('name')}>Dessert (100g serving)</TableCell>
-                            <TableCell onClick={() => this.sortBy('calories')} align="right">Calories</TableCell>
-                            <TableCell onClick={() => this.sortBy('fat')} align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell onClick={() => this.sortBy('carbs')} align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell onClick={() => this.sortBy('protein')} align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                    </TableHead>
+                    <EnhancedTableHead
+                        headCells={this.state.headCells}
+                        order={this.state.order}
+                        orderBy={this.state.orderBy}
+                        onRequestSort={this.handleRequestSort}
+                        rowCount={this.state.rows.length}
+                    />
                     <TableBody>
-                        {this.state.rows.map((row, i) => (
-                            <TableRow key={row.objectID}>
+                        {this.stableSort(this.state.rows, this.getComparator(this.state.order, this.state.orderBy)).map((row, i) => (
+                            <TableRow key={i}>
                                 <TableCell component="th" scope="row">
                                     {row.name}
                                 </TableCell>
---
+++ src/component/EnhancedTableHeader.js
@@ -0,0 +1,45 @@
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function EnhancedTableHead(props) {
+    const {headCells, order, orderBy, onRequestSort} = props;
+
+    const createSortHandler = property => event => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map(headCell => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                        {headCell.label}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+};
+
+export default EnhancedTableHead;
