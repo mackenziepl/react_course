fb79381f7dfe6d7d344bdcd0f36f5fa2ddeb498a
mackenziepl <mackenziepl@poczta.onet.pl>
2020-04-01 19:35:30 +0200

    exchange one more test

--- src/tests/Exchange.test.js
+++ src/tests/Exchange.test.js
@@ -26,8 +26,7 @@ test("calculates result after user types value", async () => {
             json: () => Promise.resolve(fakeRatesResponse),
         })
     });
-
-    const { findByLabelText, getByText, findByText} = render(<Exchange/>);
+    const { findByLabelText, getByText, findByText } = render(<Exchange/>);
     const input = await findByLabelText("Value:");
     fireEvent.change(input, { target: { value: 122 } });
     const button = getByText('Convert');
@@ -35,4 +34,34 @@ test("calculates result after user types value", async () => {
     fireEvent.click(button);
     const text = await findByText("EUR: " + (4.5694*122).toFixed(2));
     expect(text).toBeInTheDocument();
+});
+
+test("view result and message after user types wrong value", async () => {
+    const fakeRatesResponse =
+        [
+            {
+                rates: [
+                    {currency: "euro", code: "EUR", mid: 4.5694},
+                    {currency: "forint (Węgry)", code: "HUF", mid: 0.012533},
+                    {currency: "frank szwajcarski", code: "CHF", mid: 4.3236},
+                    {currency: "funt szterling", code: "GBP", mid: 5.1588}
+                ]
+            }
+        ];
+    jest.spyOn(window, 'fetch').mockImplementationOnce(() => {
+        return Promise.resolve({
+            json: () => Promise.resolve(fakeRatesResponse),
+        })
+    });
+    const { findByLabelText, getByText, findByText } = render(<Exchange/>);
+    const input = await findByLabelText("Value:");
+    fireEvent.change(input, { target: { value: -3 } });
+    const button = getByText('Convert');
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    const text = await findByText("EUR: 0");
+    const textError = await findByText("Wrong value...");
+
+    expect(text).toBeInTheDocument();
+    expect(textError).toBeInTheDocument();
 });
